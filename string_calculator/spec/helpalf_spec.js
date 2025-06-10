6b9be78bee2a8e99c73256415f417f8748b1389c
Samir Kouhnaward <samirxkouhnaward@gmail.com>
2025-06-11 00:22:14 +0200

    Erster Testfall

---
+++ string_calculator/spec/helpalf_spec.js
@@ -0,0 +1,13 @@
+describe('Help ALF', function () {
+	let helpAlf;
+
+	beforeEach(function () {
+		helpAlf = new HelpAlf();
+	});
+
+	it('sollte die korrekten Koordinaten [0, 0] zurückgeben, wenn das Raumschiff gefunden wird', function () {
+		const map = "X";
+		expect(helpAlf.findSpaceship(map)).toEqual([0, 0]);
+	});
+
+});
